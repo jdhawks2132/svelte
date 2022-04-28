4b776d7818d9063019b14c824d7dd5f890935c5a
snl95 <snl95@txstate.edu>
2022-04-28 11:25:25 -0500

    events, props, and event forwarding

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,49 @@
 <script>
-	export let name;
+	import Modal from './Modal.svelte';
+
+	let showModal = false;
+
+	const toggleModal = () => {
+		showModal = !showModal;
+	};
+
+	let people = [
+		{ id: 1, name: 'John', age: 21, beltColor: 'black' },
+		{ id: 2, name: 'Mary', age: 22, beltColor: 'blue' },
+		{ id: 3, name: 'Bob', age: 23, beltColor: 'red' },
+		{ id: 4, name: 'Peter', age: 24, beltColor: 'green' },
+		{ id: 5, name: 'Sara', age: 25, beltColor: 'yellow' },
+		{ id: 6, name: 'Paul', age: 26, beltColor: 'brown' },
+		{ id: 7, name: 'Jane', age: 27, beltColor: 'purple' },
+		{ id: 8, name: 'Mark', age: 28, beltColor: 'pink' },
+		{ id: 9, name: 'Linda', age: 29, beltColor: 'orange' },
+		{ id: 10, name: 'John', age: 30, beltColor: 'black' },
+	];
+
+	const handleClick = (id) => {
+		people = people.filter((person) => person.id !== id);
+	};
 </script>
 
+<Modal message="Hey, I am a prop" isPromo={true} {showModal} on:click={toggleModal} />
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={toggleModal}>Open Modal</button>
+	<div>
+		{#each people as person (person.id)}
+			<div>
+				<h2>{person.name}</h2>
+				{#if person.beltColor == 'black'}
+					<p><strong>MASTER NINJA</strong></p>
+				{/if}
+				<p>{person.age} years old</p>
+				<p>{person.beltColor} belt</p>
+				<button on:click={() => handleClick(person.id)}>Delete</button>
+			</div>
+		{:else}
+			<p>No people found</p>
+		{/each}
+	</div>
 </main>
 
 <style>
---
+++ src/Modal.svelte
@@ -0,0 +1,36 @@
+<script>
+	export let message = 'not a prop';
+	export let showModal = true;
+	export let isPromo = false;
+</script>
+
+{#if showModal}
+	<div class="backdrop" on:click>
+		<div class="modal" class:promo={isPromo}>
+			<p>{message}</p>
+		</div>
+	</div>
+{/if}
+
+<style>
+	.backdrop {
+		width: 100%;
+		height: 100%;
+		position: fixed;
+		background: rgba(0, 0, 0, 0.8);
+	}
+
+	.modal {
+		padding: 10px;
+		border-radius: 10px;
+		max-width: 400px;
+		margin: 10% auto;
+		text-align: center;
+		background: white;
+	}
+
+	.promo {
+		background: #ff3e00;
+		color: white;
+	}
+</style>
